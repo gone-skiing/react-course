a66b05ff8c50614edb9e59e5d2b963f1d58f1e27
Eugene Krylov <eugene@eugenekrylov.com>
2019-02-20 20:44:27 -0500

    With radium.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        persons: [
+            {id: '1', name: 'Max', age: 28},
+            {id: '2', name: 'Bob', age: 50},
+            {id: '3', name: "Pete", age: 30}
+        ],
+        showPersons: false
+    };
+
+    deletePersonHandler = (index) => {
+        // const persons = this.state.persons.slice();
+        const persons = [...this.state.persons];
+        persons.splice(index, 1);
+        this.setState({persons: persons});
+    };
+
+    nameChangedHandler = (event, id) => {
+        const personIndex = this.state.persons.findIndex((p) => {return id === p.id});
+
+        const personsCopy = [...this.state.persons];
+        personsCopy[personIndex].name = event.target.value;
+        this.setState({persons: personsCopy});
+    };
+
+    togglePersonsHandler = () => {
+        this.setState({showPersons: !this.state.showPersons});
+    };
+
+    render() {
+        const style = {
+            backgroundColor: 'green',
+            color: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer',
+            ':hover': {
+                backgroundColor: 'lightgreen',
+                color: 'black'
+            }
+        };
+
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    {this.state.persons.map((p, index) => {
+                        return (
+                            <Person
+                                key={p.id}
+                                click={() => this.deletePersonHandler(index)}
+                                changed={(event) => this.nameChangedHandler(event, p.id)}
+                                name={p.name}
+                                age={p.age}/>
+                        );
+                    })}
+                </div>
+            );
+
+            style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                color: 'white'
+            };
+        }
+
+        const classes = [];
+
+        if (this.state.persons.length <= 2) {
+            classes.push('red');
+        }
+
+        if (this.state.persons.length <= 1) {
+            classes.push('red', 'bold');
+        }
+
+        return (
+            <StyleRoot>
+                <div className="App">
+                    <h1>Hi, I am react app...</h1>
+                    <p className={classes.join(' ')}>This is really working</p>
+
+                    <button
+                        style={style}
+                        onClick={this.togglePersonsHandler}>Toggle Names</button>
+
+                    {persons}
+                </div>
+            </StyleRoot>
+        );
+//    return React.createElement('div', {className: 'App'}, React.createElement('h1', null,'How about now, I am react App from element'));
+    }
 }
 
-export default App;
+export default Radium(App);
---
+++ src/Person/Person.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Radium from 'radium';
+
+import './Person.css';
+
+const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    };
+    return (
+        <div className={"Person"} style={style}>
+            <p onClick={props.click}>I am {props.name} and I am {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type={"test"} onChange={props.changed} value={props.name}/>
+        </div>
+    );
+};
+
+// const randomAge = () => {
+//     return Math.floor(Math.random() * 30);
+// };
+
+export default Radium(person);
